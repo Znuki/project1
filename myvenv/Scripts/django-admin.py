58af52e65971eeba37bd866b228d232cb1cce733
znuki <wlsdnrdl99@naver.com>
2020-04-04 20:53:32 +0900

    firsst commit of wordcount project

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\a\desktop\project1\first_assignment\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
